551e51e416018568bd03fbfbce941f6b8b2e842d
HugoCapitan <hmoravilez@gmail.com>
2016-04-26 10:10:25 -0500

    url path logic on server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var http = require("http");
+var url = require("url");
 
 function iniciar() {
 	function onRequest(request, response) {
-		console.log("+ Peticion recibida.");
+		var pathname = url.parse(request.url).pathname;
+		console.log("+ Peticion recibida para " + pathname + " recibida.");
 		response.writeHead(200, {"Content-Type": "text/html"});
 		response.write("Hola Mundo!");
 		response.end();
