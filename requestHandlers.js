46316e0d074916c72140e445cb54f05842f7af75
HugoCapitan <hmoravilez@gmail.com>
2016-04-26 12:20:21 -0500

    hardcore command for exec doesnt blocks other handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,16 +3,19 @@ var exec = require("child_process").exec;
 function start(response) {
 	console.log("+ Request Handler 'start' was called.");
 
-	exec("ls -lah", function (error, stdout, tderr) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
-		response.end();
-	});
+	exec("find /",
+		{timeout: 10000, maxBuffer: 20000*1024},
+		function (error, stdout, stderr) {
+			response.writeHead(200, {"Content-Type": "text/plain"});
+			response.write(stdout);
+			response.end();
+		});
 }
 
 function upload(response) {
 	console.log("+ Request Handler 'upload' was called.");
 	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello upload");
 	response.end();
 }
 
