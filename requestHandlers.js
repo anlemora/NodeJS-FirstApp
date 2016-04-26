539a800d104d194ab2a38e413fa581a5ccf28e3c
HugoCapitan <hmoravilez@gmail.com>
2016-04-26 11:10:49 -0500

    using a sleep to learn the difference between blocking and non-blocking ops

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,14 @@
 function iniciar() {
 	console.log("+ El Request Handler 'iniciar' ha sido llamado.");
+
+	function sleep(miliSeconds) {
+		//Obtener la hora actual
+		var startTime = new Date().getTime();
+		//Deteniendo el CPU
+		while (new Date().getTime < startTime + miliSeconds);
+	}
+
+	sleep(10000);
 	return "Hola iniciar";
 }
 
